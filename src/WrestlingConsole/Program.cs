d0f29922f69f30bf39a50e7a72fcafed24ace207
Eric Koens <ekoens.homelab@gmail.com>
2026-04-22 10:45:17 -0500

    Wire up WrestlingConsole to create a Person from user input

--- src/WrestlingConsole/Program.cs
+++ src/WrestlingConsole/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WrestlingCore;
+
+Console.Write("Name: ");
+string name = Console.ReadLine()!;
+Console.Write("Age: ");
+int age = int.Parse(Console.ReadLine()!);
+Console.Write("Height (inches): ");
+int height = int.Parse(Console.ReadLine()!);
+
+Person person = new Person(name, age, height);
+Console.WriteLine($"Name: {person.Name}");
+Console.WriteLine($"Age: {person.Age}");
+Console.WriteLine($"Height: {person.Height} inches");
