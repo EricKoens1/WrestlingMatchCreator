b7080bfe44bdde2100f3bf61ddb56224f9aeed56
Eric Koens <ekoens.homelab@gmail.com>
2026-04-22 10:37:55 -0500

    built person class

--- src/WrestlingCore/Person.cs
+++ src/WrestlingCore/Person.cs
@@ -2,5 +2,16 @@ namespace WrestlingCore;
 
 public class Person
 {
-    // properties and constructors
+    // properties
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public int Height { get; set; }
+
+    // default constructor
+    public Person(string name, int age, int height)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Height = height;
+    }
 }
